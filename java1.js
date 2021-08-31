8be4be8ed0b388b447b4e5ea1720204277a448b2
patricia12ivan <89767611+patricia12ivan@users.noreply.github.com>
2021-08-31 11:50:15 +0300

    Create java1.js
    I did an exercise with which to get the even numbers between 1 and 30

---
+++ java1.js
@@ -0,0 +1,49 @@
+document.getElementById //take the element
+document.getElementById.innerHTML//modifies the text 
+document.getElementById.src//changes the photo when you click on it
+document.getElementById.style//changes the style
+
+window.alert(not working) //apare in the top of the file
+console.log//to see if the code of your webpage is working 
+document.write//all your changes in HTML will dissapear, usually not
+
+let a,b,c;
+a=5;
+b=1;
+c=document.getElementById();
+
+var a;
+var a="something";
+
+
+//data types
+//string
+//numbers
+//boolean -true/false 
+//string(var a="text ")
+
+//operators
+//+ you need "hello" + ' ' + "world" to get hello world if not you get "helloworld"
+//-
+// *(multiplication)
+//** (ridicare la putere)
+// /(divide)
+// %
+// ++(increment)
+// --(decrement)
+
+// ==(equally)
+// ===(same things and same types -str4ing, number, boolean)
+// !=(not equal)
+// !== (not equal or not the same type)
+// > >= <= <
+ 
+
+//logical operators
+// && (and)
+// ||(or)
+// !(not)
+
+//OBJECT
+//var car{color= ; size= ;mark= ;}
+
